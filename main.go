525e8f0cdf690b41c667e43dd83d08519b34aee6
Dmitry <d.a.demkin@gmail.com>
2025-07-04 19:25:58 +0500

    решение задачи

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
+
+func LenOFString(words string) {
+	fmt.Println("Количество символов в строке", len(words))
+}
+
+func WordsCount(words string) {
+	newWords := strings.TrimSpace(words)
+	count := strings.Count(newWords, " ")
+	fmt.Println("количество слов: ", count+1)
+}
+
+func IncomingString(words string) {
+	fmt.Println("Изначальная строка была: ", words)
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	word := "Prosto stroka v kode"
+	go func() {
+		defer wg.Done()
+		LenOFString(word)
+	}()
+	go func() {
+		defer wg.Done()
+		WordsCount(word)
+	}()
+	go func() {
+		defer wg.Done()
+		IncomingString(word)
+	}()
+
+	wg.Wait()
+
+}
